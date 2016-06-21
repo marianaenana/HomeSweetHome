eeb07a553b476557aa8599b9c13deee291c919a2
marianaenana <m.sanchezcamarena@gmail.com>
2016-06-20 21:51:49 -0500

    Cambios para pagina

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+confirm("¿Esto funcionará?");
+if ("true".length <7) { console.log ("¡Hola!"); };
+
+"¡Hola como estas!".substring(0,5);
+
+var myName = "Mariana"
+console.log(myName.substring(0,3));
+
+var deliciousFood = function (food) {
+	console.log("Me encanta comer" + "" + food);
+}
+
+deliciousFood("tacos al pastor")
